184610e6ad35db2567546b486340eb6eac6666d5
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    garnet: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638

--- extract-files.py
+++ extract-files.py
@@ -83,6 +83,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libcamxcommonutils.so',
     'vendor/lib64/libmialgoengine.so',
     ): blob_fixup().add_needed('libprocessgroup_shim.so'),
+    ('vendor/lib64/libagm.so', 'vendor/lib64/libar-pal.so', 'vendor/lib64/libaudioroute_ext.so', 'vendor/lib64/libkaraokepal.so', 'vendor/lib64/libmcs.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy': blob_fixup()
         .add_line_if_missing('setsockopt: 1'),
         .regex_replace(r'=(\d+)>', r'="\1">'),
